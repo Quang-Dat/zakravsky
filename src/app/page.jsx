fa3a2da0688e559a2bb76ee6c2bcb7b7363fd5b2
Dat Le Quang <lequangdat06@seznam.cz>
2025-02-22 11:52:03 +0100

    feat: moje prace bez obrazku

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,9 +10,9 @@ export default function Home() {
       <main>
         <section
           id="uvod"
-          className="w-full h-full flex flex-col-reverse items-center md_2:flex-row"
+          className="w-full h-full flex flex-col-reverse items-center lg:flex-row"
         >
-          <div className="bg-black w-full h-full flex flex-col items-center md_2:w-[60%] py-10">
+          <div className="bg-black w-full h-full flex flex-col items-center lg:w-[60%] pt-10 pb-36">
             <h1 className="text-white flex flex-col items-center sm:items-start text-6xl text-center">
               <span className="sm:ml-16">Designuju</span>
               <span className="text-modra font-bold sm:ml-40">
@@ -23,7 +23,7 @@ export default function Home() {
                 s vaší cílovou skupinou
               </span>
             </h1>
-            <p className="text-white text-center my-6 w-[90%] lg:w-[80%] xl:w-[820px]">
+            <p className="text-white text-center my-6 w-[90%] lg:w-[80%] max-w-[820px] z-20">
               Nikdy nebyl lepší čas 🕰️ na to pustit se do podnikání nebo
               budování vlastní značky. Každý den však vznikají tisíce nových
               značek 🆕, takže být originální je opravdová výzva. Ale nebojte, s
@@ -34,13 +34,64 @@ export default function Home() {
               videohovor. 
             </p>
             <Link
-              className={`my-3 md_2:ml-5 md_2:my-0 py-2 px-5 rounded-xl bg-zluta text-white `}
+              className={`py-2 px-5 rounded-xl bg-zluta text-white font-bold z-20`}
               href="/"
             >
               Napište mi
             </Link>
           </div>
           <div className="bg-gray-500 md_2:w-[40%] w-full h-full"></div>
+        </section>
+        <section
+          id="prace"
+          className="flex flex-col items-center justify-center w-full h-full relative"
+        >
+          <div className="flex flex-col items-center py-14">
+            <h2 className="text-center text-4xl mb-7 z-20">
+              Moje <span className="font-bold text-modra">Práce</span>
+            </h2>
+            <Link
+              className={`py-2 px-5 rounded-xl bg-black text-white font-bold z-20`}
+              href="/"
+            >
+              Další práce
+            </Link>
+          </div>
+          <svg
+            className="absolute -top-40 -left-[400px] z-10"
+            width="817"
+            height="650"
+            viewBox="0 0 817 650"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M717.062 225.791L408.271 325.271C237.714 380.218 54.9381 286.491 0 115.96L308.791 16.48C479.322 -38.4581 662.116 55.2339 717.062 225.791Z"
+              fill="#3FA9F5"
+            />
+            <path
+              d="M816.542 534.582L507.751 634.062C337.194 689.009 154.418 595.282 99.48 424.751L408.271 325.271C578.802 270.333 761.596 364.025 816.542 534.582Z"
+              fill="#3FA9F5"
+            />
+          </svg>
+
+          <svg
+            className="absolute -top-40 -right-[400px] z-10"
+            width="817"
+            height="650"
+            viewBox="0 0 817 650"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M99.4808 225.791L408.272 325.271C578.829 380.218 761.605 286.49 816.543 115.96L507.752 16.4797C337.221 -38.4584 154.427 55.2337 99.4808 225.791Z"
+              fill="#3FA9F5"
+            />
+            <path
+              d="M0.000792956 534.582L308.792 634.062C479.349 689.009 662.125 595.282 717.063 424.751L408.272 325.271C237.741 270.333 54.9474 364.025 0.000792956 534.582Z"
+              fill="#3FA9F5"
+            />
+          </svg>
         </section>
       </main>
     </>
